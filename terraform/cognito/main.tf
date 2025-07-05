cdeb2fb8c92f5a107038e2dd59c3a595dd2121f9
Malyik József <jozsef.malyik@barre.hu>
2025-07-05 11:20:40 +0200

    cognito tf format fix

--- terraform/cognito/main.tf
+++ terraform/cognito/main.tf
@@ -2,21 +2,21 @@ resource "aws_cognito_user_pool" "main" {
   name = "mss-user-pool"
 
   auto_verified_attributes = ["email"]
-  alias_attributes        = ["email"]
-  username_attributes    = ["email"]
+  alias_attributes         = ["email"]
+  username_attributes      = ["email"]
 
   schema {
-    attribute_data_type      = "String"
-    name                    = "email"
-    required                = true
-    mutable                 = true
+    attribute_data_type = "String"
+    name                = "email"
+    required            = true
+    mutable             = true
   }
 
   schema {
-    attribute_data_type      = "String"
-    name                    = "profile_image_url"
-    required                = false
-    mutable                 = true
+    attribute_data_type = "String"
+    name                = "profile_image_url"
+    required            = false
+    mutable             = true
   }
 
   admin_create_user_config {
@@ -41,8 +41,8 @@ resource "aws_cognito_user_pool_client" "main" {
     "ALLOW_USER_OAUTH_FLOW"
   ]
 
-  allowed_oauth_flows = ["code", "implicit"]
-  allowed_oauth_scopes = ["email", "openid", "profile"]
+  allowed_oauth_flows                  = ["code", "implicit"]
+  allowed_oauth_scopes                 = ["email", "openid", "profile"]
   allowed_oauth_flows_user_pool_client = true
 
 
