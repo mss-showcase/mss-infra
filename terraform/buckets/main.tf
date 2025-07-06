868f56c277aa8963686d150774c84c7de157dcbb
Malyik József <jozsef.malyik@barre.hu>
2025-07-06 12:36:31 +0200

    webhosting block access policy disable

--- terraform/buckets/main.tf
+++ terraform/buckets/main.tf
@@ -40,8 +40,8 @@ resource "aws_s3_bucket_website_configuration" "webhosting" {
 
 resource "aws_s3_bucket_public_access_block" "webhosting" {
   bucket                  = aws_s3_bucket.webhosting.id
-  block_public_acls       = true
-  block_public_policy     = true
+  block_public_acls       = false
+  block_public_policy     = false
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
