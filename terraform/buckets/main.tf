1325ba39b95f5a79c1f1cc5a8469050e4db8c4ef
Malyik József <jozsef.malyik@barre.hu>
2025-07-06 13:41:49 +0200

    turning off all webhosting bucket blocking rules

--- terraform/buckets/main.tf
+++ terraform/buckets/main.tf
@@ -42,8 +42,8 @@ resource "aws_s3_bucket_public_access_block" "webhosting" {
   bucket                  = aws_s3_bucket.webhosting.id
   block_public_acls       = false
   block_public_policy     = false
-  ignore_public_acls      = true
-  restrict_public_buckets = true
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
 
 # to be able to test the website without cloudfront, we need to allow public read access 
