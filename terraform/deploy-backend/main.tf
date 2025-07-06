4480b261db3b82e7b1753ca5986a7882fbec6154
Malyik József <jozsef.malyik@barre.hu>
2025-07-06 14:52:23 +0200

    cors

--- terraform/deploy-backend/main.tf
+++ terraform/deploy-backend/main.tf
@@ -75,7 +75,6 @@ resource "aws_apigatewayv2_api" "mss_backend_api" {
   cors_configuration {
     allow_origins = [
       "http://localhost:5173",
-      "http://${var.webhosting_website_url}",
       "https://${var.webhosting_website_url}",
       "https://${var.cloudfront_domain_name}"
     ]
