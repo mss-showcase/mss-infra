bd6ee42a3b173c4d6135a687b626f1fd08c52f41
Malyik József <jozsef.malyik@barre.hu>
2025-07-07 19:28:32 +0200

    routing fix

--- terraform/cloudfront/cloudfront.tf
+++ terraform/cloudfront/cloudfront.tf
@@ -81,13 +81,17 @@ resource "aws_cloudfront_distribution" "cf_distribution" {
     max_ttl     = 86400
   }
 
-  # Custom error page for SPA routing
+  # Custom error pages for SPA routing
   custom_error_response {
     error_code         = 404
     response_code      = 200
     response_page_path = "/index.html"
   }
-
+  custom_error_response {
+    error_code         = 403
+    response_code      = 200
+    response_page_path = "/index.html"
+  }
   restrictions {
     geo_restriction {
       restriction_type = "none"
