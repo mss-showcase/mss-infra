98cb06e184a1bf8a5e95bde0e772801fbf029a1d
Malyik József <jozsef.malyik@barre.hu>
2025-07-07 15:36:34 +0200

    cognito wf updated

--- terraform/cognito/main.tf
+++ terraform/cognito/main.tf
@@ -72,19 +72,3 @@ resource "aws_cognito_identity_provider" "google" {
     name  = "name"
   }
 }
-
-# Outputs for debugging and other services
-output "user_pool_id" {
-  description = "ID of the Cognito User Pool"
-  value       = aws_cognito_user_pool.main.id
-}
-
-output "user_pool_client_id" {
-  description = "ID of the Cognito User Pool Client"
-  value       = aws_cognito_user_pool_client.main.id
-}
-
-output "google_identity_provider_name" {
-  description = "Name of the Google Identity Provider"
-  value       = aws_cognito_identity_provider.google.provider_name
-}
